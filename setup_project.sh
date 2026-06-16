5e66b9bb4a5eb444210c7e0a16f3cb3575facb67
jgattetekez-code <j.gatetekez@alustudent.com>
2026-06-16 12:54:33 +0200

    my first block which is going to ask for the user for the input and create the directories for the attendance tracker

--- setup_project.sh
+++ setup_project.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+echo "Enter project name:"
+read project_name
+
+if [ -d "attendance_tracker_$project_name" ]; then
+        echo "Directory already exists."
+        exit 1
+fi
+
+mkdir "attendance_tracker_$project_name"
+
+echo "Main folder created successfully.i"
+
+mkdir "attendance_tracker_$project_name/Helpers"
+mkdir "attendance_tracker_$project_name/reports"
+
+echo "project structure created successfully."
+
+cp project/attendance_checker.py "attendance_tracker_$project_name/"
+
+cp project/assets.csv "attendance_tracker_$project_name/Helpers/"
+
+cp project/config.json "attendance_tracker_$project_name/Helpers/"
+
+cp project/reports.log "attendance_tracker_$project_name/reports/"
